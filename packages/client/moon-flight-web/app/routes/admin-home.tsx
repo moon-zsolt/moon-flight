971749ab9552eff55d2c22f125e3908a381cc30d
Zsolt Világos <zsolt.vilagos@moon42.com>
2025-04-24 12:00:55 +0200

    tailor home to admin/user site selector

--- packages/client/moon-flight-web/app/routes/admin-home.tsx
+++ packages/client/moon-flight-web/app/routes/admin-home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Moon Flight Home" },
-    { name: "description", content: "Welcome to Moon Flight!" },
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
   ];
 }
 

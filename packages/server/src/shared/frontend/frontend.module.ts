f3e45c15f5d7a255140a73adf09894cdd433974f
Zsolt Világos <zsolt.vilagos@moon42.com>
2025-04-28 22:19:18 +0200

    configure server-static for production usage

--- packages/server/src/shared/frontend/frontend.module.ts
+++ packages/server/src/shared/frontend/frontend.module.ts
@@ -1,11 +1,28 @@
-import { DynamicModule, Module } from "@nestjs/common";
+import { DynamicModule, Module, ModuleMetadata } from "@nestjs/common";
 import { FrontendProxyModule } from "./frontend-proxy.module";
+import {
+  ServeStaticModule,
+  ServeStaticModuleOptions,
+} from "@nestjs/serve-static";
+import { join } from "path";
 
 @Module({})
 export class FrontendModule {
   static register(): DynamicModule {
+    const imports: ModuleMetadata["imports"] = [];
+
+    if (process.env.DEV) {
+      imports.push(FrontendProxyModule);
+    } else {
+      const options: ServeStaticModuleOptions = {
+        rootPath: join(__dirname, "../../../../client/build/client"),
+      };
+
+      imports.push(ServeStaticModule.forRoot(options));
+    }
+
     return {
-      imports: [FrontendProxyModule],
+      imports,
       module: FrontendModule,
       providers: [],
     };

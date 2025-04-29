c4ee914af0ca45a77e9227c180c3c15541003c82
Zsolt Világos <zsolt.vilagos@moon42.com>
2025-04-29 15:53:58 +0200

    icons

--- packages/client/app/pages/admin/admin-home/admin-home.tsx
+++ packages/client/app/pages/admin/admin-home/admin-home.tsx
@@ -1,3 +1,4 @@
+import { MapPinHouse, Plane, Tickets } from "lucide-react";
 import { NavLink } from "react-router";
 
 export function AdminHome() {
@@ -32,71 +33,16 @@ const functions = [
   {
     href: "./destinations",
     text: "Destinations",
-    icon: (
-      // https://lucide.dev/icons/map-pin-house
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="lucide lucide-map-pin-house-icon lucide-map-pin-house"
-      >
-        <path d="M15 22a1 1 0 0 1-1-1v-4a1 1 0 0 1 .445-.832l3-2a1 1 0 0 1 1.11 0l3 2A1 1 0 0 1 22 17v4a1 1 0 0 1-1 1z" />
-        <path d="M18 10a8 8 0 0 0-16 0c0 4.993 5.539 10.193 7.399 11.799a1 1 0 0 0 .601.2" />
-        <path d="M18 22v-3" />
-        <circle cx="10" cy="10" r="3" />
-      </svg>
-    ),
+    icon: <MapPinHouse />,
   },
   {
     href: "./flights",
     text: "Flights",
-    icon: (
-      // https://lucide.dev/icons/plane
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="lucide lucide-plane-icon lucide-plane"
-      >
-        <path d="M17.8 19.2 16 11l3.5-3.5C21 6 21.5 4 21 3c-1-.5-3 0-4.5 1.5L13 8 4.8 6.2c-.5-.1-.9.1-1.1.5l-.3.5c-.2.5-.1 1 .3 1.3L9 12l-2 3H4l-1 1 3 2 2 3 1-1v-3l3-2 3.5 5.3c.3.4.8.5 1.3.3l.5-.2c.4-.3.6-.7.5-1.2z" />
-      </svg>
-    ),
+    icon: <Plane />,
   },
   {
     href: "./bookings",
     text: "Bookings",
-    icon: (
-      // https://lucide.dev/icons/tickets
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="lucide lucide-tickets-icon lucide-tickets"
-      >
-        <path d="m4.5 8 10.58-5.06a1 1 0 0 1 1.342.488L18.5 8" />
-        <path d="M6 10V8" />
-        <path d="M6 14v1" />
-        <path d="M6 19v2" />
-        <rect x="2" y="8" width="20" height="13" rx="2" />
-      </svg>
-    ),
+    icon: <Tickets />,
   },
 ];

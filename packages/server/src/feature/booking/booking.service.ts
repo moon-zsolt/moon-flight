e4d688ca15da9766460163806a9e16f4d3c75626
Péter Polena <peter.polena@moon42.com>
2025-04-30 00:07:32 +0200

    fix empty seats calculation and saving

--- packages/server/src/feature/booking/booking.service.ts
+++ packages/server/src/feature/booking/booking.service.ts
@@ -71,8 +71,9 @@ export class BookingService {
 
     booking.checkedIn = 1;
     booking.seat = await this.getSeat(booking.flight);
-    booking.flight.emptySeats = -1;
+    booking.flight.emptySeats -= 1;
 
+    await this.flightRepository.save(booking.flight);
     await this.bookingRepository.save(booking);
 
     return await this.bookingRepository.findOneOrFail({

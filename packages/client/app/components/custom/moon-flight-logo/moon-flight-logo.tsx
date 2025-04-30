0f1a70e817dac9f00e3177ded65ce4a052c87884
Zsolt Világos <zsolt.vilagos@moon42.com>
2025-04-30 08:41:13 +0200

    link home

--- packages/client/app/components/custom/moon-flight-logo/moon-flight-logo.tsx
+++ packages/client/app/components/custom/moon-flight-logo/moon-flight-logo.tsx
@@ -5,16 +5,18 @@ export function MoonFlightLogo() {
   return (
     <header className="flex flex-col items-center gap-9">
       <div className="w-[300px] lg:w-[500px] lg:max-w-[100vw] p-4">
-        <img
-          src={logoLight}
-          alt="Moon Flight Logo Light"
-          className="block w-full dark:hidden"
-        />
-        <img
-          src={logoDark}
-          alt="Moon Flight Logo Dark"
-          className="hidden w-full dark:block"
-        />
+        <a href="/">
+          <img
+            src={logoLight}
+            alt="Moon Flight Logo Light"
+            className="block w-full dark:hidden"
+          />
+          <img
+            src={logoDark}
+            alt="Moon Flight Logo Dark"
+            className="hidden w-full dark:block"
+          />
+        </a>
       </div>
     </header>
   );
